ae4637988e6f70df64bdc4a147bea3ff54ba658d
Inkyy06 <fboranunal@gmail.com>
2026-07-24 17:39:32 +0300

    Added comments and modified a variable's name

--- MonteCarloPi.cpp
+++ MonteCarloPi.cpp
@@ -3,32 +3,37 @@
 
 int main(){
 
-    int points;
+    int N;
     int count = 0;
     double Total_Points_Inside_Circle = 0;
     double MonteCarloPi;
 
-    //Random number engine X
+    //Random number engine
     std::mt19937 engine(std::random_device{}());
     //random number distribution x
     std::uniform_real_distribution<double> distX(-1.0, 1.0);
     //random number distribution Y
     std::uniform_real_distribution<double> distY(-1.0, 1.0);
-    
+
+    //accepts user input
     do{
         std::cout << "Enter the amount of points: ";
-        std::cin >> points;
+        std::cin >> N;
 
-        if(points <= 0){
+        if(N <= 0){
             std::cout << "Please enter a positive value!\n";
         }
-    }while(points <= 0);
+    
+    }while(N <= 0);
 
-    while(count < points){
+    //generates random N(user input) points 
+    while(count < N){
+        //generates a point
         double x = distX(engine);
         double y = distY(engine);
         std::cout << "Point " << count + 1 << ": x = "<< x << " y = " << y << '\n';
 
+        //checks if the point is in circle
         if(x * x + y * y <= 1){
             Total_Points_Inside_Circle++;
         }
@@ -36,8 +41,9 @@ int main(){
         count++;
     }
 
+    //prints the estimated pi
     MonteCarloPi = 4 * (Total_Points_Inside_Circle/count);
-    std::cout << "PI = " << MonteCarloPi << '\n'; 
+    std::cout << "ESTIMATED PI = " << MonteCarloPi << '\n'; 
     
     return 0;
 }
