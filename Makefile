b8d6120a8a75633c99fc77104fa1f73230219913
Inkyy06 <fboranunal@gmail.com>
2026-07-24 19:26:15 +0300

    Parallelized with OpenMP, edited some comments, added makefile, modified .vscode

---
+++ Makefile
@@ -0,0 +1,2 @@
+MonteCarloPi: MonteCarloPi.cpp
+	/opt/homebrew/bin/g++-16 -fdiagnostics-color=always -g MonteCarloPi.cpp -o MonteCarloPi -fopenmp
--- MonteCarloPi.cpp
+++ MonteCarloPi.cpp
@@ -1,19 +1,12 @@
 #include <iostream>
 #include <random>
+#include <omp.h>
 
 int main(){
 
     int N;
-    int count = 0;
     double Total_Points_Inside_Circle = 0;
     double MonteCarloPi;
-
-    //Random number engine
-    std::mt19937 engine(std::random_device{}());
-    //random number distribution x
-    std::uniform_real_distribution<double> distX(-1.0, 1.0);
-    //random number distribution Y
-    std::uniform_real_distribution<double> distY(-1.0, 1.0);
 
     //accepts user input
     do{
@@ -26,23 +19,33 @@ int main(){
     
     }while(N <= 0);
 
-    //generates random N(user input) points 
-    while(count < N){
-        //generates a point
-        double x = distX(engine);
-        double y = distY(engine);
-        std::cout << "Point " << count + 1 << ": x = "<< x << " y = " << y << '\n';
+    /*  generates random N(user input) points, 
+        and keeps track of total number of points and number of points inside the circle */
+    #pragma omp parallel
+    {
 
-        //checks if the point is in circle
-        if(x * x + y * y <= 1){
-            Total_Points_Inside_Circle++;
+        //Random number engine
+        std::mt19937 engine(std::random_device{}() + omp_get_thread_num());
+        //random number distribution X
+        std::uniform_real_distribution<double> distX(-1.0, 1.0);
+        //random number distribution Y
+        std::uniform_real_distribution<double> distY(-1.0, 1.0);
+
+        #pragma omp for reduction(+:Total_Points_Inside_Circle)    
+        for(int count = 0; count < N; count++){
+            //generates a point
+            double x = distX(engine);
+            double y = distY(engine);
+
+            //checks if the point is in circle
+            if(x * x + y * y <= 1){
+                Total_Points_Inside_Circle++;
+            }
         }
-
-        count++;
     }
 
     //prints the estimated pi
-    MonteCarloPi = 4 * (Total_Points_Inside_Circle/count);
+    MonteCarloPi = 4 * (Total_Points_Inside_Circle/N);
     std::cout << "ESTIMATED PI = " << MonteCarloPi << '\n'; 
     
     return 0;
