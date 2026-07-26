41b5e469f6d367af25a2bc98c843d481b9a082ab
Inkyy06 <fboranunal@gmail.com>
2026-07-26 03:48:35 +0300

    changed variable names, edited comments

--- MonteCarloPi.cpp
+++ MonteCarloPi.cpp
@@ -4,21 +4,21 @@
 
 int main(){
 
-    int N;
-    double Total_Points_Inside_Circle = 0;
-    double MonteCarloPi;
+    int Total_Points;
+    double Points_Inside_Circle = 0;
+    double PI_ESTIMATE;
     double start_time, end_time, elapsed_time;
 
     //reads user input if the value is positive
     do{
         std::cout << "Enter the amount of points: ";
-        std::cin >> N;
+        std::cin >> Total_Points;
 
-        if(N <= 0){
+        if(Total_Points <= 0){
             std::cout << "Please enter a positive value!\n";
         }
     
-    }while(N <= 0);
+    }while(Total_Points <= 0);
 
     start_time = omp_get_wtime();
 
@@ -29,20 +29,20 @@ int main(){
 
         //Random number engine
         std::mt19937 engine(std::random_device{}() + omp_get_thread_num());
-        //random number distribution X
+        //X coordiante distribution
         std::uniform_real_distribution<double> distX(-1.0, 1.0);
-        //random number distribution Y
+        //Y coordinate distribution
         std::uniform_real_distribution<double> distY(-1.0, 1.0);
 
         #pragma omp for reduction(+:Total_Points_Inside_Circle)    
-        for(int count = 0; count < N; count++){
+        for(int count = 0; count < Total_Points; count++){
             //generates a random point
             double x = distX(engine);
             double y = distY(engine);
 
             //checks if the point is in circle
             if(x * x + y * y <= 1){
-                Total_Points_Inside_Circle++;
+                Points_Inside_Circle++;
             }
         }
     }
@@ -53,9 +53,9 @@ int main(){
     elapsed_time = end_time - start_time;
 
     //The estimated pi
-    MonteCarloPi = 4 * (Total_Points_Inside_Circle/N);
+    PI_ESTIMATE = 4 * (Points_Inside_Circle/Total_Points);
 
-    std::cout << "ESTIMATED PI = " << MonteCarloPi << '\n'; 
+    std::cout << "ESTIMATED PI = " << PI_ESTIMATE << '\n'; 
     std::cout << "Passed Time = " << elapsed_time << " seconds" << '\n';
     
     return 0;
